a15543491b80f04b3f949923222bf041ee82f89f
Max Graham <m.huw.graham@gmail.com>
2019-01-14 23:51:55 +0000

    Added some error throws to handle if repo owner and/or repo name are not entered OR if they are not valid and cannot be found.

--- download-avatars.js
+++ download-avatars.js
@@ -2,29 +2,31 @@ var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 
-console.log('Welcome to the GitHub Avatar Downloader');
-
 var input = process.argv.slice(2);
 var repoOwner = input[0];
 var repoName = input[1];
 
 //Make sure two fields have been entered. Call function if true.
 if (repoOwner === undefined || repoName === undefined) {
-  console.log("Please enter both the repo owner, and the repo name.");
-} else {
-
-  getRepoContributors(repoOwner, repoName, function(err, result) {
-
-    console.log("Errors:", err);
-    for (var contributor of result){ //Iterate through the JSON object
-      var filePath = './avatars/'; //file path to download images
-
-      //Download images from their URL to a designated file path.
-      //Name the files after the user's login name.
-      downloadImageByURL(contributor.avatar_url, filePath + contributor.login);
-    }
-  });
+  throw new Error("Please enter the repo owner and then the repo name.");
 }
+
+console.log('Starting downloads...');
+
+getRepoContributors(repoOwner, repoName, function(err, result) {
+
+  for (var contributor of result){
+  //Iterate through the JSON object
+
+    var filePath = './avatars/';
+    //file path to download images
+
+    downloadImageByURL(contributor.avatar_url, filePath + contributor.login);
+    //Download images from their URL to a designated file path.
+    //Name the files after the user's login name.
+  }
+});
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -37,7 +39,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    var results = JSON.parse(body); //Parse body into readable JSON
+
+    if(res.statusCode !== 200){
+      throw new Error("Cannot access Github repo. Check that you entered the correct repo and owner.");
+    }
+    //Check to make sure the data is accessed.
+    //Throw an error if it is not accessable.
+
+    var results = JSON.parse(body);
+    //Parse body into JSON
     cb(err, results);
   });
 }
@@ -50,12 +60,13 @@ function downloadImageByURL(url, filePath) {
       throw err;
     })
     .on('response', function(response){
-      console.log('Response Status Code: ', response.statusCode);
 
-    response.on('end', function(){
-      console.log("Download Complete!");
-     });
+      response.on('end', function(){
+        console.log("Download Complete!");
+        //Notify user when an image has been downloaded.
+      });
 
     })
     .pipe(fs.createWriteStream(filePath));
+    //Downloads image to set directory
 };
