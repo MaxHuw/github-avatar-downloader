0aa4a3bdd1971ad211a0de0de9936df1a53e75f5
Max Graham <m.huw.graham@gmail.com>
2019-01-14 21:06:41 +0000

    secrets.js created and used.

--- download-avatars.js
+++ download-avatars.js
@@ -1,9 +1,18 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
+
+
   request(url, function(err, res, body) {
     cb(err, body);
   });
